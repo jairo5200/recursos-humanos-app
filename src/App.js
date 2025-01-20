69798b2daf72ec578c1c168863fcbe3edaed68d3
jairo5200 <jairo5200@gmail.com>
2025-01-20 16:26:31 -0500

    creamos el proyecto de react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import listadoEmpleados from "./empleados/ListadoEmpleados";
+
+
+function App() {
+  return (
+    <ListadoEmpleados>
+      
+    </ListadoEmpleados>
+  );
+}
+
+export default App;
---
+++ src/empleados/ListadoEmpleados.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ListadoEmpleados() {
+  return (
+    <div className="container text-center" style={{margin: "30px"}}>
+      <h3>Sistema de Recursos Humanos</h3>
+    </div>
+  )
+}
+
