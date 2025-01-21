51ec9ad5a3f14b9baca7d8e7961538acc69e8538
jairo5200 <jairo5200@gmail.com>
2025-01-20 19:00:04 -0500

    implementamos el formulario de crear empleado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ListadoEmpleados from "./empleados/ListadoEmpleados";
 import Navegacion from "./plantilla/Navegacion";
+import AgregarEmpleado from "./empleados/AgregarEmpleado";
 
 function App() {
   return (
     <div className="container">
+      <BrowserRouter>
       <Navegacion></Navegacion>
-      <ListadoEmpleados></ListadoEmpleados>
+      <Routes>
+        <Route path="/" element={<ListadoEmpleados />} />
+        <Route path="/agregar" element={<AgregarEmpleado/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
-    
   );
 }
 
